f65ad54033a667094122514b987554c1e8a937c1
Alex <a.rodriguezpic@alumnos.urjc.es>
2019-03-18 18:36:52 +0100

    Añadido librerías Sparkfun Signed-off-by: Alex <a.rodriguezpic@alumnos.urjc.es>

---
+++ Arduino/Test_BME280/Test_BME280.ino
@@ -0,0 +1,28 @@
+
+#include "SparkFunBME280.h"
+BME280 mySensor;
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.print("Humidity: ");
+  Serial.print(mySensor.readFloatHumidity(), 0);
+
+  Serial.print(" Pressure: ");
+  Serial.print(mySensor.readFloatPressure(), 0);
+
+  Serial.print(" Alt: ");
+  //Serial.print(mySensor.readFloatAltitudeMeters(), 1);
+  Serial.print(mySensor.readFloatAltitudeFeet(), 1);
+
+  Serial.print(" Temp: ");
+  //Serial.print(mySensor.readTempC(), 2);
+  Serial.print(mySensor.readTempF(), 2);
+
+  Serial.println();
+
+  delay(50);
+}
