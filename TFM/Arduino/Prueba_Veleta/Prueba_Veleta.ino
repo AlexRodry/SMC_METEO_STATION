23d28c7b48a063072867f06094025985bf5f542e
Alex <a.rodriguezpic@alumnos.urjc.es>
2019-07-02 00:30:10 +0200

    Añadido programa de la veleta funcional

--- TFM/Arduino/Prueba_Veleta/Prueba_Veleta.ino
+++ TFM/Arduino/Prueba_Veleta/Prueba_Veleta.ino
@@ -1,9 +1,46 @@
-void setup() {
-  // put your setup code here, to run once:
 
+const int sensorPin = A1; // seleccionar la entrada para el sensor
+int sensorValue;          // Variable que almacena el valor raw (0 a 1023)
+String veleta;            // Dirección que marca la veleta 
+void setup()
+{
+   Serial.begin(9600);
+   Serial.println("Test de Veleta"); 
+   Serial.println("Raw Value\tDirección"); 
+}
+ 
+void loop() 
+{
+   sensorValue = analogRead(sensorPin);       // Realiza la lectura
+   veleta = direction(sensorValue);           // Se procesa el valor Raw con una función
+   delay(1000);
+   Serial.print(sensorValue); Serial.print("\t\t"); Serial.println(veleta); 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+String direction(int a)
+{
+  if ((a>=225)&&(a<=400)){
+    return "N";
+  }
+  else if ((a>=501)&&(a<=675)){
+    return "NE";
+  }
+  else if ((a>=676)&&(a<=800)){
+    return "S";
+  }
+  else if ((a>=801)&&(a<=895)){
+    return "SE";
+  }
+  else if (a>=895){
+    return "E";
+  }
+  else if ((a>=401)&&(a<=500)){
+    return "SO";
+  }
+  else if (a<=140){
+    return "O";
+  }
+  else if ((a>=141)&&(a<=224)){
+    return "NO";
+  }
 }
